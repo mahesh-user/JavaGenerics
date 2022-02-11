44770e23dbe321dfe083f03062d38acd4f6225c6
Mahesh <mahesh51138@gmail.com>
2022-02-11 21:52:41 +0530

    added:MaximuumNumber

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -1,30 +1,38 @@
 package com.bridgelabz;
 
 
-    public class MaximumTest<T extends Comparable> {
-        //class level variables
-        public T x,y,z;
+public class MaximumTest<T extends Comparable> {
+    //class level variables
+    public T x,y,z;
 
-
-        /**
-         *passing three objects as parameters0 to get maximum number
-         * @param x
-         * @param y
-         * @param z
-         * @return maximum
-         */
-        T maximumOfObjects(T x,T y, T z){
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            T maximum = null;
-            if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
-                maximum = x;
-            if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
-                maximum = y;
-            if (z.compareTo(x) > 0 && z.compareTo(y) > 0)
-                maximum = z;
-            return maximum;
-        }
+    /**
+     * parameterised constructor to assign values to the instance variables
+     * @param x
+     * @param y
+     * @param z
+     */
+    MaximumTest(T x,T y,T z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
+
+    /**
+     *passing three objects as parameters to get maximum number
+     * @return maximum
+     */
+    T maximum(){
+        T maximum = null;
+        if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
+            maximum = x;
+        else if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
+            maximum = y;
+        else
+            maximum = z;
+        return maximum;   //returning largest one
+    }
+    public <T> void printMax(){
+        System.out.println("Maximum number is : " + this.maximum());
+    }
+}
--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -1,10 +1,9 @@
 package com.bridgelabz;
 
 public class MaximumValue {
-
-        public static void main(String[] args) {
-            MaximumTest maximumTest = new MaximumTest();
-            System.out.println("Maximum of the three integers is : " +maximumTest.maximumOfObjects(2.5d,3.4d,4d));
-        }
+    public static void main(String[] args) {
+        MaximumTest maximumTest = new MaximumTest(56,345,346);
+        maximumTest.printMax();
+    }
     }
 
