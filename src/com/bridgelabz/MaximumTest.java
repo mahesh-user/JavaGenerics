247e4f0c443b9e1b0a27ebffbd39951320e2cffe
Mahesh <mahesh51138@gmail.com>
2022-02-11 23:07:05 +0530

    added:FindingTheMaximumValue

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -1,36 +1,28 @@
 package com.bridgelabz;
 
 
-public class MaximumTest<T extends Comparable> {
+public class MaximumTest {
     //class level variables
-    public T x,y,z;
+    public Integer x,y,z;
 
     /**
-     * parameterised constructor to assign values to the instance variables
+     *passing three integers as parameters0 to get maximum number
      * @param x
      * @param y
      * @param z
+     * @return maximum
      */
-    MaximumTest(T x,T y,T z){
+    public int maximumOfIntegers(Integer x,Integer y, Integer z){
         this.x = x;
         this.y = y;
         this.z = z;
-    }
-
-
-    /**
-     *passing three objects as parameters to get maximum number
-     * @return maximum
-     */
-    T maximumOfObjects(T x,T y,T z){
-        T maximum = null;
+        int maximum=0;
         if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
             maximum = x;
-        else if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
+        if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
             maximum = y;
-        else
+        if (z.compareTo(x) > 0 && z.compareTo(y) > 0)
             maximum = z;
-        return maximum;   //returning largest one
+        return maximum;
     }
-
 }
--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -1,8 +1,10 @@
 package com.bridgelabz;
 
 public class MaximumValue {
-    public static void main(String[] args) {
 
-    }
+        public static void main(String[] args) {
+            MaximumTest maximumTest = new MaximumTest();
+            System.out.println("Maximum of the three integers is : " +maximumTest.maximumOfIntegers(5,6,7));
+        }
     }
 
