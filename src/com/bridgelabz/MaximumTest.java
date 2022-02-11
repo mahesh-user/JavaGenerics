9c17432e3425958ff91e54bccad38033494a82f7
Mahesh <mahesh51138@gmail.com>
2022-02-12 01:48:07 +0530

    refactored:CreateGenericclass

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -2,15 +2,10 @@ package com.bridgelabz;
 
 
 public class MaximumTest<T extends Comparable> {
-    //class level variables
+
     public T x,y,z;
 
-    /**
-     * parameterised constructor to assign values to the instance variables
-     * @param x
-     * @param y
-     * @param z
-     */
+
     MaximumTest(T x,T y,T z){
         this.x = x;
         this.y = y;
@@ -18,10 +13,7 @@ public class MaximumTest<T extends Comparable> {
     }
 
 
-    /**
-     *passing three objects as parameters to get maximum number
-     * @return maximum
-     */
+
     T maximumOfObjects(T x,T y,T z){
         T maximum = null;
         if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
@@ -30,7 +22,7 @@ public class MaximumTest<T extends Comparable> {
             maximum = y;
         else
             maximum = z;
-        return maximum;   //returning largest one
+        return maximum;
     }
 
 }
