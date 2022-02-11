19d6300b4ae2a7ae58ba6cced22cca424c496333
Mahesh <mahesh51138@gmail.com>
2022-02-11 23:12:13 +0530

    added:FindingTheMaximumString

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -2,9 +2,10 @@ package com.bridgelabz;
 
 
 public class MaximumTest {
-    //class level variables
+
     public Integer x,y,z;
     public Float a,b,c;
+    public String s1,s2,s3;
 
     /**
      *passing three integers as parameters0 to get maximum number
@@ -45,6 +46,27 @@ public class MaximumTest {
             maximum = b;
         if (c.compareTo(a) > 0 && c.compareTo(b) > 0)
             maximum = c;
+        return maximum;
+    }
+
+    /**
+     * Comparing the three strings and returning the maximum string
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return maximum
+     */
+    String maximumOfStrings(String s1,String s2, String s3){
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+        String maximum = null;
+        if(s1.compareTo(s2) > 0 && s1.compareTo(s3) > 0)
+            maximum = s1;
+        else if (s2.compareTo(s1) > 0 && s2.compareTo(s3) > 0)
+            maximum = s2;
+        else
+            maximum = s3;
         return maximum;
     }
 }
--- src/com/bridgelabz/MaximumValue.java
+++ src/com/bridgelabz/MaximumValue.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 public class MaximumValue {
     public static void main(String[] args) {
         MaximumTest maximumTest = new MaximumTest();
-        System.out.println("Maximum of the three integers is : " +maximumTest.maximumOfFloats(5.4f,8.4f,7.6f));
+        System.out.println("Maximum of the three integers is : " +maximumTest.maximumOfStrings("apple","waterMelon","Banana"));
     }
 }
-
