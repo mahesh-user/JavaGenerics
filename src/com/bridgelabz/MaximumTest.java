f46527af6ed7727b219e4d53a83c774b76acdf57
Mahesh <mahesh51138@gmail.com>
2022-02-12 01:42:05 +0530

    refactored:MaximumGenericMethod

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -5,12 +5,7 @@ public class MaximumTest<T extends Comparable> {
     //class level variables
     public T x,y,z;
 
-    /**
-     * parameterised constructor to assign values to the instance variables
-     * @param x
-     * @param y
-     * @param z
-     */
+
     MaximumTest(T x,T y,T z){
         this.x = x;
         this.y = y;
@@ -18,10 +13,7 @@ public class MaximumTest<T extends Comparable> {
     }
 
 
-    /**
-     *passing three objects as parameters to get maximum number
-     * @return maximum
-     */
+
     T maximum(){
         T maximum = null;
         if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
